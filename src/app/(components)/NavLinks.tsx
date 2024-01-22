2d44f688f258b1ffffb4dbfa51f5880856895e38
StevenMODIMO <stevepetes26@gmail.com>
2024-01-22 11:14:29 +0300

    v1: Developer Portfolio

---
+++ src/app/(components)/NavLinks.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+
+const NavLinks = () => {
+  return (
+    <nav></nav>
+  )
+}
+
+export default NavLinks
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import { Russo_One } from "next/font/google";
+import Image from "next/image";
+
+const russoOne = Russo_One({ subsets: ["cyrillic"], weight: "400" });
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="font-['Russo one'] text-8xl text-[#FFFFFF]">
-        Unlocking Possibilities through code
-      </h1>
+    <main className={`${russoOne.className} flex flex-col gap-10 py-4`}>
+      <div>
+        <h1 className="text-center text-5xl text-[#FFFFFF]">
+          Unlocking Possibilities through code
+          <span className="flex justify-center w-fit mx-auto p-1 rounded mt-2">
+            <Image
+              alt="teminal-icon"
+              src="/terminal.svg"
+              width={50}
+              height={50}
+            />
+          </span>
+        </h1>
+      </div>
+      <p className="text-center text-lg text-[#DEC544]">
+        Welcome to my digital realm, where creativity meets functionality. Join
+        me on a journey of Innovation and Problem Solving
+      </p>
     </main>
   );
 }
