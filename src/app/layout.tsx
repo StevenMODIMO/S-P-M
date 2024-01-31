6eef37ad189f6c3985c40bf13308c71f66a6c10f
StevenMODIMO <stevepetes26@gmail.com>
2024-01-31 21:30:17 +0300

    Steven Modimo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className + "w-fit h-fit bg-black"}>
         <Navbar />
-        {children}  
+        {children}
       </body>
     </html>
   );
