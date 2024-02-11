436bab23a2a333f03f25366807b08a763735e21b
StevenMODIMO <stevepetes26@gmail.com>
2024-02-11 20:10:18 +0300

    v1: Developer Portfolio

--- src/app/(components)/NavLinks.tsx
+++ src/app/(components)/NavLinks.tsx
@@ -2,9 +2,10 @@
 import React from "react";
 import Link from "next/link";
 import Footer from "./Footer";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { FaTimes } from "react-icons/fa";
 import navlinks from "../data/data";
+import Image from "next/image";
 
 interface NavLinksProps {
   setOpenLinks: React.Dispatch<React.SetStateAction<boolean>>;
@@ -12,36 +13,40 @@ interface NavLinksProps {
 
 const NavLinks: React.FC<NavLinksProps> = ({ setOpenLinks }) => {
   return (
-    <AnimatePresence>
-      <motion.main
-        initial={{ x: -400 }}
-        animate={{ x: 0 }}
-        exit={{ x: 400 }}
-        transition={{
-          ease: "easeInOut",
-          x: { duration: 0.3 },
-          delayChildren: 0.3,
-          staggerChildren: 0.2,
-        }}
-        className="bg-black/90 absolute top-0 left-0 w-screen h-screen flex flex-col justify-between p-3 md:hidden"
-      >
-        <nav className="text-[#DEC544] flex flex-col gap-3 text-xl">
-          <div className="flex justify-end">
-            <FaTimes onClick={() => setOpenLinks(false)} />
-          </div>
-          {navlinks.map((link) => (
-            <Link
-              onClick={() => setOpenLinks(false)}
-              href={link.path}
-              key={link.id}
-            >
-              {link.title}
-            </Link>
-          ))}
-        </nav>
-        <Footer />
-      </motion.main>
-    </AnimatePresence>
+    <motion.main
+      initial={{ x: -400 }}
+      animate={{ x: 0 }}
+      exit={{ x: -400 }}
+      transition={{
+        ease: "easeInOut",
+        x: { duration: 0.3 },
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      }}
+      className="bg-black/95 absolute top-0 left-0 w-screen h-screen flex flex-col justify-between p-3 md:hidden"
+    >
+      <nav className="text-[#DEC544] flex flex-col gap-3 text-xl">
+        <div className="flex justify-between">
+          <Image
+            src="/StevenMODIMO.svg"
+            alt="steven-modimo-logo"
+            width={150}
+            height={100}
+          />
+          <FaTimes onClick={() => setOpenLinks(false)} />
+        </div>
+        {navlinks.map((link) => (
+          <Link
+            onClick={() => setOpenLinks(false)}
+            href={link.path}
+            key={link.id}
+          >
+            {link.title}
+          </Link>
+        ))}
+      </nav>
+      <Footer />
+    </motion.main>
   );
 };
 
--- src/app/(components)/Navbar.tsx
+++ src/app/(components)/Navbar.tsx
@@ -5,24 +5,32 @@ import Image from "next/image";
 import { RiBarChartHorizontalLine } from "react-icons/ri";
 import NavLinks from "./NavLinks";
 import navlinks from "../data/data";
+import { AnimatePresence } from "framer-motion";
 
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
   const [openLinks, setOpenLinks] = useState(false);
   return (
-    <nav className="fixed top-0 left-0 w-full bg-black">
-      <header className="p-3 flex justify-between items-center m-1">
+    <nav className="fixed top-0 left-0 w-full bg-black bg-opacity-5">
+      <header
+        className={
+          openLinks
+            ? "p-3 flex justify-between items-center m-1 opacity-5"
+            : "p-3 flex justify-between items-center m-1"
+        }
+      >
         <Link href="/">
-          <Image src="/StevenMODIMO.svg" alt="steven-modimo-logo" width={150} height={100} />
+          <Image
+            src="/StevenMODIMO.svg"
+            alt="steven-modimo-logo"
+            width={150}
+            height={100}
+          />
         </Link>
         <RiBarChartHorizontalLine
           onClick={() => setOpenLinks(true)}
-          className={
-            openLinks
-              ? "text-[#DEC544] text-2xl bg-black opacity-5 md:hidden"
-              : "text-[#DEC544] text-2xl bg-black md:hidden"
-          }
+          className="text-[#DEC544] text-2xl md:hidden"
         />
         <section className="hidden md:text-[#DEC544] text-lg md:flex gap-2">
           {navlinks.map((link) => (
@@ -36,7 +44,9 @@ const Navbar: React.FC<NavbarProps> = () => {
           ))}
         </section>
       </header>
-      {openLinks && <NavLinks setOpenLinks={setOpenLinks} />}
+      <AnimatePresence>
+        {openLinks && <NavLinks setOpenLinks={setOpenLinks} />}
+      </AnimatePresence>
     </nav>
   );
 };
