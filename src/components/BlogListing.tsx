b8331d69d27426f78a3c5b973d9df4155735bd47
StevenMODIMO <stevepetes26@gmail.com>
2025-08-11 19:05:04 +0300

    v2: Developer Portfolio

--- src/components/BlogListing.tsx
+++ src/components/BlogListing.tsx
@@ -1,5 +1,21 @@
 import { blogPlaceholders } from "@/lib/data";
+import Container from "./Container";
 
 export default function BlogListing() {
-  return <div>BlogListing</div>;
+  return (
+    <Container>
+      <div className="flex flex-col gap-2">
+        {blogPlaceholders.map((blog) => (
+          <div key={blog.id} className="bg-[#FAFAFA] flex flex-col px-4 py-2">
+            <img src={blog.image} alt={blog.title} />
+            <h2 className="text-4xl font-medium">{blog.title}</h2>
+            <p className="text-lg font-medium">{blog.description}</p>
+            <span className="font-bold text-sm">
+              {new Date(blog.createdAt).toLocaleDateString()}
+            </span>
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
 }
