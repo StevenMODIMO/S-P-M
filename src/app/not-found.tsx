250f89059d6ee0ed73d62e7083b3e8b99c3e5634
StevenMODIMO <stevepetes26@gmail.com>
2025-08-12 09:56:16 +0300

    v2: Developer Portfolio

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,32 @@
+"use client";
+import Link from "next/link";
+import { House } from "lucide-react";
+import { motion } from "motion/react";
+import { useRouter } from "next/navigation";
+
 export default function NotFound() {
+  const router = useRouter();
   return (
-    <main className="mt-24 text-[#DEC544] text-center text-xl">
-      <p>404: Page Not Found</p>
+    <main className="mt-24 text-[#393a1f] dark:text-[#E7EE1A] text-center text-xl">
+      <motion.p
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        404: Page Not Found
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        onClick={() => router.back()}
+        className="cursor-pointer hover:underline hover:text-[#E7EE1A] dark:hover:text-[#9CA600]"
+      >
+        <div className="flex items-center justify-center gap-2 mt-4">
+          <House className="h-6 w-6" />
+          <span>Go to Home</span>
+        </div>
+      </motion.div>
     </main>
   );
 }
