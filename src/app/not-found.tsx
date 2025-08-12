7d0023ff93586f7c5e9b651610f4e7b0cd384bde
StevenMODIMO <stevepetes26@gmail.com>
2025-08-12 09:58:33 +0300

    v2: Developer Portfolio

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/navigation";
 export default function NotFound() {
   const router = useRouter();
   return (
-    <main className="mt-24 text-[#393a1f] dark:text-[#E7EE1A] text-center text-xl">
+    <main className="mt-24 text-[#393a1f] dark:text-[#E7EE1A] text-center text-xl lg:text-4xl">
       <motion.p
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
@@ -20,7 +20,7 @@ export default function NotFound() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
         onClick={() => router.back()}
-        className="cursor-pointer hover:underline hover:text-[#E7EE1A] dark:hover:text-[#9CA600]"
+        className="cursor-pointer hover:underline hover:text-[#E7EE1A] dark:hover:text-[#9CA600] lg:text-3xl"
       >
         <div className="flex items-center justify-center gap-2 mt-4">
           <House className="h-6 w-6" />
