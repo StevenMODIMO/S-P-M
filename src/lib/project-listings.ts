4d323e856721ec863abe022ab459c611ffce5c16
StevenMODIMO <stevepetes26@gmail.com>
2025-08-24 20:26:02 +0300

    v2: Developer Portfolio

---
+++ src/lib/project-listings.ts
@@ -0,0 +1,10 @@
+interface ProjectListingsTypes {
+  id: number;
+  title: string;
+  description: string;
+  demoLink: string;
+  githubLink: string;
+  skill: string;
+  stack: string;
+  category: string;
+}
