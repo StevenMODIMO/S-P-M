fa8ce64638128b5d725f80d009d74fd9942c9727
StevenMODIMO <stevepetes26@gmail.com>
2025-08-25 09:35:02 +0300

    v2: Developer Portfolio

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,13 +7,7 @@ export const metadata: Metadata = {
   description: "Explore my projects and contributions to the tech community.",
 };
 
-export default function Projects({
-  searchParams,
-}: {
-  searchParams: { q?: string; sort?:string };
-}) {
-  const query = searchParams.q || "No query provided";
-  const sort = searchParams.sort || "No query provided";
+export default function Projects() {
   return (
     <main className="h-full">
      
--- src/components/ProjectsFilter.tsx
+++ src/components/ProjectsFilter.tsx
@@ -1,11 +1,4 @@
 "use client";
-import { useSearchParams } from "next/navigation";
 export default function ProjectsFilter() {
-  const searchParams = useSearchParams();
-  const query = searchParams.get("q") || "No query provided";
-  const sort = searchParams.get("sort") || "No query provided";
-  return (
-    <div>
-    </div>
-  );
+  return <div></div>;
 }
