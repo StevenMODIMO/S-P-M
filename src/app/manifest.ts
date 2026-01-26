4231e062f2df44ff39dda52bbee861aeb923110a
StevenMODIMO <stevepetes26@gmail.com>
2026-01-26 21:22:35 +0300

    Added PWA functionality

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "StevenMODIMO",
+    short_name: "SPM",
+    description: "Steven Modimo's personal portfolio website - PWA Version",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#0000000",
+    icons: [
+      {
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
