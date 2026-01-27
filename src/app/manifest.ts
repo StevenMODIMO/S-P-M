810ee909df98c27387e2405d6c43d60d45bfaa3c
StevenMODIMO <stevepetes26@gmail.com>
2026-01-27 09:55:01 +0300

    cotask version 1

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,7 +8,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
-    theme_color: "#0000000",
+    theme_color: "#000000",
     icons: [
       {
         src: "/web-app-manifest-192x192.png",
