db8369ca7011d9541051cfa57aede7f6799e4cbc
StevenMODIMO <stevepetes26@gmail.com>
2026-01-30 15:46:34 +0300

    cotask version 1

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -5,7 +5,7 @@ export default function manifest(): MetadataRoute.Manifest {
     name: "StevenMODIMO",
     short_name: "SPM",
     description: "Steven Modimo's personal portfolio website - PWA Version",
-    start_url: "/",
+    start_url: "/dashboard",
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#000000",
